bda41e79d70f0349a0789602b4cd019bd041218d
PhuChau <vinhphu101195@gmail.com>
2019-09-22 17:35:54 +0300

    grid with header

--- src/App.js
+++ src/App.js
@@ -5,7 +5,15 @@ import "./sass/main.scss";
 function App() {
   return (
     <div className="container">
-      <header className="header"></header>
+      <header className="header">
+        <div className="header__infor">
+          <h2 className="heading-2 ">Web Developer: </h2>
+          <h1 className="heading-1 ">Phu Chau</h1>
+          <h3 className="heading-3">Work Hard - Play Hard</h3>
+          <button class="btn header__btn">more</button>
+        </div>
+        <div className="header__avatar"></div>
+      </header>
     </div>
   );
 }
