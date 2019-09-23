898c6188be3dd05055c7873bc6724cb57d84ab3d
PhuChau <vinhphu101195@gmail.com>
2019-09-23 10:40:10 +0300

    add logo and use flex

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
 import React from "react";
+import Header from "./components/headerPage/HeaderPage";
 
 import "./sass/main.scss";
 
 function App() {
   return (
     <div className="container">
-      <header className="header">
-        <h2 className="heading-2 ">Web Developer: </h2>
-        <h1 className="heading-1 ">Vinh Phu Chau</h1>
-        <h3 className="heading-3">Work Hard - Play Hard</h3>
-        <div class="header__seenon-text">Seen on</div>
-        <div class="header__seenon-logos">
-          <img src="components/Home/logo/html-logo.jpg" alt="Seen on logo 1" />
-          <img src="img/logo-forbes.png" alt="Seen on logo 2" />
-          <img src="img/logo-techcrunch.png" alt="Seen on logo 3" />
-          <img src="img/logo-bi.png" alt="Seen on logo 4" />
-        </div>
-      </header>
+      <Header></Header>
     </div>
   );
 }
---
+++ src/components/headerPage/HeaderPage.js
@@ -0,0 +1,28 @@
+import React from "react";
+import htmllogo from "./logo/html.svg";
+import csslogo from "./logo/css.svg";
+import javascriptlogo from "./logo/javascript.svg";
+import react from "./logo/react.svg";
+
+const Home = () => {
+  return (
+    <header className="header">
+      <div className="header__top">
+        <h2 className="heading-2 ">Web Developer: </h2>
+        <h1 className="heading-1 ">Vinh Phu Chau</h1>
+        <h3 className="heading-3">Work Hard - Play Hard</h3>
+      </div>
+      <div className="header__bottom">
+        <div class="header__seenon-text">Seen on</div>
+        <div class="header__seenon-logos">
+          <img src={htmllogo} alt="Seen on logo 1" width="100rem" />
+          <img src={csslogo} alt="Seen on logo 2" width="100rem" />
+          <img src={javascriptlogo} alt="Seen on logo 3" width="100rem" />
+          <img src={react} alt="Seen on logo 4" width="100rem" />
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Home;
