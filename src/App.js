538669d6a031b70f4a601fed48f3e61e42a3bd36
PhuChau <vinhphu101195@gmail.com>
2019-09-23 16:13:21 +0300

    build profile

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Header from "./components/headerPage/HeaderPage";
+import Profile from "./components/profile/Profile";
 
 import "./sass/main.scss";
 
@@ -7,6 +8,7 @@ function App() {
   return (
     <div className="container">
       <Header></Header>
+      <Profile></Profile>
     </div>
   );
 }
--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -1,7 +1,35 @@
 import React from "react";
+import avatar from "./img/avatar.jpg";
 
 const Profile = () => {
-  return <section className="profile"></section>;
+  return (
+    <section className="profile">
+      <img
+        src={avatar}
+        alt="Seen on logo 1"
+        className="profile-avatar"
+        width="300rem"
+      />
+      <div className="profile__infor">
+        <p class="profile__text">
+          <h4 className="heading-4">Information</h4>
+          &nbsp;&nbsp;&nbsp; My name is Phu Chau, I'm studying at Turku
+          University of Applied Sciences in Finland. I have 1 years experience
+          building and maintaining responsive websites in the recruiting
+          industry.
+          <br></br>
+          <br></br>
+          Proficient in HTML, CSS, JavaScript; plus modern libraries and
+          frameworks such as React, Redux, React Router, Nodejs. Passionate
+          about learning new things and outsite activity.
+          <br></br>
+          <br></br>
+          <h4 className="heading-4">Experience</h4>
+          06.2018 - 07.2019: Student Assistant of TUAS as Web developer.
+        </p>
+      </div>
+    </section>
+  );
 };
 
 export default Profile;
