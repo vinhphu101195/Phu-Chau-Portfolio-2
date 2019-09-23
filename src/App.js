c10bd330adbee6f800d717dfc9d2a83e06eae14c
PhuChau <vinhphu101195@gmail.com>
2019-09-23 10:06:59 +0300

    create home page

--- src/App.js
+++ src/App.js
@@ -6,13 +6,16 @@ function App() {
   return (
     <div className="container">
       <header className="header">
-        <div className="header__infor">
-          <h2 className="heading-2 ">Web Developer: </h2>
-          <h1 className="heading-1 ">Phu Chau</h1>
-          <h3 className="heading-3">Work Hard - Play Hard</h3>
-          <button class="btn header__btn">more</button>
+        <h2 className="heading-2 ">Web Developer: </h2>
+        <h1 className="heading-1 ">Vinh Phu Chau</h1>
+        <h3 className="heading-3">Work Hard - Play Hard</h3>
+        <div class="header__seenon-text">Seen on</div>
+        <div class="header__seenon-logos">
+          <img src="components/Home/logo/html-logo.jpg" alt="Seen on logo 1" />
+          <img src="img/logo-forbes.png" alt="Seen on logo 2" />
+          <img src="img/logo-techcrunch.png" alt="Seen on logo 3" />
+          <img src="img/logo-bi.png" alt="Seen on logo 4" />
         </div>
-        <div className="header__avatar"></div>
       </header>
     </div>
   );
