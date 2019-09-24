72ff2d6cf76fc7019f492a925839c17e6ff5cb1c
PhuChau <vinhphu101195@gmail.com>
2019-09-24 10:53:43 +0300

    start build skills

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "./components/headerPage/HeaderPage";
 import Profile from "./components/profile/Profile";
+import Skills from "./components/skills/Skills";
 
 import "./sass/main.scss";
 
@@ -9,6 +10,7 @@ function App() {
     <div className="container">
       <Header></Header>
       <Profile></Profile>
+      <Skills></Skills>
     </div>
   );
 }
---
+++ src/components/skills/Skills.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Skills = () => {
+  return (
+    <section className="skills">
+      <h2 className="heading-2">My Skills</h2>
+    </section>
+  );
+};
+
+export default Skills;
