b1b2efe9f4efe71426014e665f5f6bda480e8177
PhuChau <vinhphu101195@gmail.com>
2019-09-30 17:20:59 +0300

    build references

--- src/components/reference/Reference.js
+++ src/components/reference/Reference.js
@@ -3,9 +3,24 @@ import React from 'react';
 const Reference = () => {
     return (
         <section className="reference">
-            <h2 className="heading-2">
-                Reference
+            <h2 className="heading-2 reference__header">
+                My References
             </h2>
+            <div className="reference__infor">
+                <p className="reference__infor__text">
+                    <p className="reference__infor__text__name "><strong> Mr. Balsam Abdulghani</strong></p>
+                    <br></br>
+                    FINCODA Software Application Manager
+                <br></br>
+                    Turku University of Applied Sciences
+                <br></br>
+                    Turku Finland
+                <br></br>
+                    balsam.abdulghani@turkuamk.fi
+                <br></br>
+                    Tel +358-449074990
+                </p>
+            </div>
 
         </section>
     );
