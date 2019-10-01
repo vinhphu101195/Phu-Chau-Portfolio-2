b44083c3615b94720ba96f4251f238ddafdfbdaf
PhuChau <vinhphu101195@gmail.com>
2019-10-01 13:02:49 +0300

    build footer

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Skills from "./components/skills/Skills";
 import Projects from "./components/projects/Projects";
 import Contact from "./components/contact/Contact";
 import Reference from './components/reference/Reference';
+import Footer from "./components/footer/Footer";
 
 import "./sass/main.scss";
 
@@ -21,6 +22,7 @@ function App() {
         <Projects></Projects>
         <Contact></Contact>
         <Reference></Reference>
+        <Footer></Footer>
       </ProjectContexts>
     </div>
   );
---
+++ src/components/footer/Footer.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <div className="footer__container">
+                <a href="https://drive.google.com/file/d/1tGndbJok4LyOsKt_uHL1ZP9vhRVq0pOa/view" className="footer__link">
+                    <i class="fa fa-download icon" aria-hidden="true"></i>
+                    My Resume
+            </a>
+                <p className="footer__text">
+                    © 2019 Phu Chau. Made in the Finland.
+                </p>
+            </div>
+
+        </footer>
+    );
+}
+
+export default Footer;
