da6bd89fa08de1d6c13e9e49adcda5da3b9b1795
PhuChau <vinhphu101195@gmail.com>
2019-10-01 13:14:02 +0300

    finish footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -4,7 +4,11 @@ const Footer = () => {
     return (
         <footer className="footer">
             <div className="footer__container">
-                <a href="https://drive.google.com/file/d/1tGndbJok4LyOsKt_uHL1ZP9vhRVq0pOa/view" className="footer__link">
+                <a href="https://drive.google.com/file/d/1tGndbJok4LyOsKt_uHL1ZP9vhRVq0pOa/view"
+                    className="footer__link"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
                     <i class="fa fa-download icon" aria-hidden="true"></i>
                     My Resume
             </a>
